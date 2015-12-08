592e2c030222b4d539d2ed759dfa6e5059b760b5
girishvi <voxrok@gmail.com>
2015-12-08 23:23:21 +0530

    Update installscripts.rb

--- installscripts.rb
+++ installscripts.rb
@@ -1,6 +1,6 @@
 window.intercomSettings = {
   email: 'joeomally@example.com', 
-  name: 'Joe O'Mally',  ##Joe O' Mally should be 'Joe "O'"Mally'
+  name: 'Joe O'Mally',  ## Joe O' Mally should be 'Joe "O'"Mally'
   app_id: 'abc1234',
   created_at: 1234567890
 }
@@ -13,7 +13,7 @@ window.intercomSettings = {
 window.intercomSettings = {
   email: 'bob@example.com',
   name: 'Bob Sagget',
-  app_id: 'abc1234' ##created_at parameter is missing 
+  app_id: 'abc1234' ## created_at parameter is missing in next line
 }
 window.intercomSettings = {
   email: 'bob@example.com',
@@ -24,7 +24,7 @@ window.intercomSettings = {
   paid_account: "true" ## paid_account should not be a string rather paid_account : true
 }
 window.intercomSettings = {
-  eamil: 'bob@example.com', ## eamil should be changed to email
+  eamil: 'bob@example.com', ## 'eamil' should be changed to 'email'
   name: 'Bob Sagget',
   app_id: 'abc1234',
   created_at: 1234567890,
