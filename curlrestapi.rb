68f61e1801d48249e5b03cab1ef5da94f539ccb2
girishvi <voxrok@gmail.com>
2015-12-08 23:43:48 +0530

    Create curlrestapi.rb

---
+++ curlrestapi.rb
@@ -0,0 +1,3 @@
+curl https://api.intercom.io/users?user_id=007 \
+-u b91mge2h:ro-6f36c8e7cb5cefe263338b9032856764f31e00a2  \
+-H 'Accept:application/json'  ## Not sure what the Correct APi Url is. Will need to replace
